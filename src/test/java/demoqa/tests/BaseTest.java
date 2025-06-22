19c2ef1602eb0fe5cba1952c9ec75c17e296390a
iProzza <59886338+iProzza@users.noreply.github.com>
2025-06-22 14:43:31 +0300

    refactoring

--- src/test/java/demoqa/tests/BaseTest.java
+++ src/test/java/demoqa/tests/BaseTest.java
@@ -1,10 +1,17 @@
 package demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import demoqa.helpers.Attach;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -14,18 +21,47 @@ public class BaseTest {
     @BeforeAll
     static void setup() {
 
+        String selenoidHost = System.getProperty("selenoid_host", "selenoid.autotests.cloud");
+        String selenoidLogin = System.getProperty("selenoid_login", "user1");
+        String selenoidPassword = System.getProperty("selenoid_password", "1234");
+        String browser = System.getProperty("browser", "chrome");
+        String browserVersion = System.getProperty("browserVersion", "127.0");
+        String screenResolution = System.getProperty("screenResolution", "1920x1080");
+
         WebDriverManager.chromedriver()
                 .clearDriverCache()
                 .clearResolutionCache()
                 .setup();
 
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = screenResolution;
+        Configuration.browser = browser;
+        Configuration.browserVersion = browserVersion;
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 10000;
+        Configuration.remote = String.format("https://%s:%s@%s/wd/hub",
+                selenoidLogin,
+                selenoidPassword,
+                selenoidHost);
         RestAssured.baseURI = "https://demoqa.com";
-//        RestAssured.basePath = "/v1";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
     }
 
     @AfterEach
     void shutDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
         closeWebDriver();
     }
 }
